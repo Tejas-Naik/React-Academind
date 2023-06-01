32b689be2f0eae49b716b79210eab79e95fb8fff
Tejas-Naik <RNTejas2005@gmail.com>
2023-05-31 19:24:57 -0700

    JavaScript refresher let vs const

---
+++ JS-refresher/script.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// Let and Const
+// use let if the variables value is going to change
+// use const if the variables value is not going to change (constant)
+
+let age = 17;
+age = 18;
+
+const birthYear = 2005;
+console.log(age);
+console.log(birthYear);
